8ba5a1973ceeb4cab8f893a62be845ca9f7dacf8
Dominique Wynn <80863089+InkiiSheepii@users.noreply.github.com>
2021-08-14 16:40:15 -0400

    commitChangesapp

--- src/App.js
+++ src/App.js
@@ -2,18 +2,16 @@ import React from 'react';
 import './App.css';
 import { useAuth0 } from "@auth0/auth0-react";
 
+  
 
 function App() {
   return (
     <div className="App">
-      <h1></h1>
+      <h1>Login Page</h1>
     </div>
   );
-
-  const {} = useAuth0();
-
+  const {loginWithRedirect} = useAuth0();
+  loginWithRedirect();
 };
-
-
 
 export default App;
